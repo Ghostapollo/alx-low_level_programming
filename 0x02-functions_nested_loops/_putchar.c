1d9324f7e52d331d061fbd81ecc1b1def528ffcf
Angel <Angel>
2023-02-21 15:48:11 -0800

    putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,11 +3,11 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
- *
+ * @c: The character to print
  * Return: On success 1.
- *On error, -1 is returend, and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putcahr(char c)
 {
 	return (write(1, &c, 1));
