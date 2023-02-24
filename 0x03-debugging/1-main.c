7aca2570471286571714624978b3b418eca79e55
Angel <Angel>
2023-02-23 17:45:15 -0800

    like comment subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,18 +10,11 @@ int  main(void)
 	/*int i;*/
 
 	printf("Infinite loop incoming :(\n");
-
 	/*1=0;*/
-
 	/*while (i < 10)*/
-
 	/*(*/
-
 	/*       putchar(i);*/
-	
 	/*)*/
-	
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
