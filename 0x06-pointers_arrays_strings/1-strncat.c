263a0dd2d9c4dcde8f0a6bccf4e3dec095981fc3
Angel <Angel>
2023-03-02 17:27:22 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - Concatentes two strings using at most
+ * _strncat - Concatenates two strings using at most
  *
  * an imputted number of bytes from src.
  *
@@ -21,6 +21,6 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[index++])
 	dest_len++;
 	for (index = 0; src[index] && index < n; index++)
-	dest [dest_len++] = src[index];
+	dest[dest_len++] = src[index];
 	return (dest);
 }
