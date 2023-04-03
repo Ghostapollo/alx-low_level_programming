2162c182a76a58a0ff60a4cd987bc7056c38c1f2
Angel <Angel>
2023-04-03 16:06:14 -0700

    reverse list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,19 +8,18 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
+	listint_t *current, *next;
+
+	current = *head;
+	*head = NULL;
 
 	while (current != NULL)
 	{
 		next = current->next;
-		current->next = prev;
-		prev = current;
+		current->next = *head;
+		*head = current;
 		current = next;
 	}
-
-	*head = prev;
 
 	return (*head);
 }
