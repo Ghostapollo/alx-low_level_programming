a7f28ff1b58a90d230f10511698d6409367ddaf0
Angel <Angel>
2023-04-09 15:07:45 -0700

    101 password

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * get_endianness - checks the endianness
+ * get_endianness - checks the endianness of the system
  *
  * Returns: 0 if big endian, 1 if little endian
  */
